c8891913088168f91f939453fd0b37cac6771658
capibersuit <christianferro79@gmail.com>
2015-11-16 23:08:36 -0300

    otro mas

---
+++ supermercado/src/ar/gov/chris/client/pantalla/PantallaInicio.java
@@ -0,0 +1,39 @@
+package ar.gov.chris.client.pantalla;
+
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.Button;
+
+public class PantallaInicio extends Pantalla {
+
+	Button btn_productos;
+	Button btn_listas;
+	
+	public PantallaInicio() {
+		super();
+		pantalla_principal();
+		// TODO Auto-generated constructor stub
+	}
+
+	private void pantalla_principal() {
+		
+		btn_productos= new Button("Productos");
+		btn_listas= new Button("Listas");
+		panel.add(btn_productos);
+		panel.add(btn_listas);
+		agregar_listener();
+	}
+
+	private void agregar_listener() {
+		btn_listas.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				panel.clear();
+				History.newItem("PantallaListas");}
+		});
+	}
+	
+	
+
+}
