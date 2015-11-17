10824c2183fb2b23c038758544d514efae7a10da
capibersuit <christianferro79@gmail.com>
2015-11-17 13:27:25 -0200

    creo la clase producto (desde el MECON!)

---
+++ supermercado/src/ar/gov/chris/server/clases/Producto.java
@@ -0,0 +1,49 @@
+package ar.gov.chris.server.clases;
+
+public class Producto {
+	
+	int id;
+	String nombre;
+	//String descripcion;
+	float precio;
+	public Producto() {
+		
+	}
+	public Producto(int id, String nombre, float precio) {
+		
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+	public Producto(String nombre, float precio) {
+		
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", precio="
+				+ precio + "]";
+	}
+}
