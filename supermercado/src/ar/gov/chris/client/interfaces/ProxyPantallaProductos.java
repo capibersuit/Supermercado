1de3de9df2e908ce8e7adcbfee1707e4d06744db
capibersuit <christianferro79@gmail.com>
2015-12-01 20:01:40 -0300

    Agrego una excepcion.

--- supermercado/src/ar/gov/chris/client/interfaces/ProxyPantallaProductos.java
+++ supermercado/src/ar/gov/chris/client/interfaces/ProxyPantallaProductos.java
@@ -1,11 +1,12 @@
 package ar.gov.chris.client.interfaces;
 
 import ar.gov.chris.client.datos.DatosProducto;
+import ar.gov.chris.client.gwt.excepciones.GWT_ExcepcionBD;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 
 public interface ProxyPantallaProductos extends RemoteService {
 
-	void agregar_producto(DatosProducto datos_prod);
+	void agregar_producto(DatosProducto datos_prod) throws GWT_ExcepcionBD;
 
 }
