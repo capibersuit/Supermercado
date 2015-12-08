eb170b382b71fd3572c3e60192c6498681528950
capibersuit <christianferro79@gmail.com>
2015-12-08 19:33:31 -0300

    Agrego el archivo para generar la BD.

---
+++ supermercado/src/BD/supermercado.sql
@@ -0,0 +1,27 @@
+-- Table: productos
+
+-- DROP TABLE productos;
+
+CREATE TABLE productos
+(
+  id serial NOT NULL,
+  nombre text,
+  precio double precision,
+  CONSTRAINT id_pk_productos PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE productos
+  OWNER TO postgres;
+
+
+CREATE TABLE listas
+(
+  id serial NOT NULL,
+  comentario text,
+  fecha timestamp,
+  CONSTRAINT id_pk_listas PRIMARY KEY (id )
+)
+
+SELECT NOW();
