bcc988937a5def0af8ee7f9a554919c495dcea7e
capibersuit <christianferro79@gmail.com>
2015-12-14 17:55:32 -0300

    Empiezo con la pantalla de vista de compra

--- supermercado/src/ar/gov/chris/client/pantalla/PantallaVistaDeCompra.java
+++ supermercado/src/ar/gov/chris/client/pantalla/PantallaVistaDeCompra.java
@@ -1,11 +1,27 @@
 package ar.gov.chris.client.pantalla;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+
+import ar.gov.chris.client.interfaces.ProxyPantallaListas;
+import ar.gov.chris.client.interfaces.ProxyPantallaListasAsync;
+
 public class PantallaVistaDeCompra extends Pantalla {
+
+	private ProxyPantallaListasAsync proxy_listas;
+
 
 	public PantallaVistaDeCompra() {
 		super();
+		inicializar();
 	}
 	
 	
-
+	/** Se crea el proxy_carga para comunicarse con el servidor.
+	 */
+	protected void inicializar(){
+		this.proxy_listas= (ProxyPantallaListasAsync)
+		GWT.create(ProxyPantallaListas.class);
+		super.inicializar((ServiceDefTarget) this.proxy_listas, "Listas");
+	}
 }
