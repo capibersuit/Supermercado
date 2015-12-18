ed2f6f295cf33db3ec0495ef4cc04ece8939e327
capibersuit <christianferro79@gmail.com>
2015-12-17 22:15:32 -0300

    Modificaciones varias para agregar el boton de borrado y su funcionalidad.

--- supermercado/src/ar/gov/chris/client/widgets/WidgetMostrarProductos.java
+++ supermercado/src/ar/gov/chris/client/widgets/WidgetMostrarProductos.java
@@ -3,11 +3,17 @@ package ar.gov.chris.client.widgets;
 import java.util.Set;
 
 import ar.gov.chris.client.datos.DatosProducto;
+import ar.gov.chris.client.pantalla.Pantalla;
+import ar.gov.chris.client.pantalla.PantallaVistaDeCompra;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PushButton;
 
 public class WidgetMostrarProductos extends Composite {
 
@@ -15,6 +21,15 @@ public class WidgetMostrarProductos extends Composite {
 	private FlexTable lista_prod;
 	private Label titulo_label;
 	private int next_row;
+	private int next_col=0;
+	private Pantalla parent;
+	
+	private DatosProducto prod_actual;
+
+	
+	private PushButton btn_borrar;
+	private WidgetConfirmar confirmar_borrado;
+
 	
 	/** Crea un {@link WidgetMostrarLineas} a partir de los par�metros.
 	 * 
@@ -22,7 +37,9 @@ public class WidgetMostrarProductos extends Composite {
 	 * @param titulo T�tulo del widget.
 	 */
 	public WidgetMostrarProductos(final Set<DatosProducto> lista, 
-			String titulo) {
+			String titulo, int num_compra, Pantalla parent) {
+		
+		this.parent= parent;
 		principal= new FlowPanel();
 		lista_prod= new FlexTable();
 		titulo_label= new Label(titulo);
@@ -31,26 +48,52 @@ public class WidgetMostrarProductos extends Composite {
 		Label nombre_prod_label= new Label("Producto");
 		Label precio_label= new Label("Precio");
 		Label cant_label= new Label("Cantidad");
-//		Label modelo_label= new Label("Modelo");
-//		Label nro_serie_label= new Label("Nro de serie");
+		Label total_label= new Label("Precio total");
+		Label borrar_label= new Label("Borrar");
 //		Label opciones_label= new Label("Opciones");
 		
-		lista_prod.setWidget(0, 0, nombre_prod_label);
-		lista_prod.setWidget(0, 1, precio_label);
-		lista_prod.setWidget(0, 2, cant_label);
-//		lista_lineas.setWidget(0, 3, modelo_label);
-//		lista_lineas.setWidget(0, 4, nro_serie_label);
+		lista_prod.setWidget(0, next_col, nombre_prod_label);
+		next_col++;
+		lista_prod.setWidget(0, next_col, precio_label);
+		next_col++;
+		lista_prod.setWidget(0, next_col, cant_label);
+		next_col++;
+		if(titulo.equalsIgnoreCase("Vista de compra")) {
+			lista_prod.setWidget(0, next_col, total_label);
+			next_col++;
+		}
+		lista_prod.setWidget(0, next_col, borrar_label);
+		if(!titulo.equalsIgnoreCase("Vista de compra")) {
+			lista_prod.setWidget(0, next_col, borrar_label);
+//			next_col++;
+		}
 //		lista_lineas.setWidget(0, 5, opciones_label);
 		
 		next_row= 1; 
 
 		for (DatosProducto prod : lista) {
-//			CheckBox check_quitar= new CheckBox("Quitar l�nea");
-			lista_prod.setText(next_row, 0, prod.getNombre());
-			lista_prod.setText(next_row, 1, String.valueOf(prod.getPrecio()));
-			lista_prod.setText(next_row, 2, ((prod.getCantidad()!=0) ? String.valueOf(prod.getCantidad()) : "NA"));
-//		lista_lineas.setText(next_row, 2, prod.obtener_marca());
-//		lista_lineas.setText(next_row, 3, prod.obtener_modelo());
+			next_col=0;
+			
+			btn_borrar= new PushButton("Borrar");
+
+			prod_actual= prod;
+			agregar_handler();
+
+			
+			lista_prod.setText(next_row, next_col, prod.getNombre());
+			next_col++;
+			lista_prod.setText(next_row, next_col, String.valueOf(prod.getPrecio()));
+			next_col++;
+			lista_prod.setText(next_row, next_col, ((prod.getCantidad()!=0) ? String.valueOf(prod.getCantidad()) : "NA"));
+			next_col++;
+			if(titulo.equalsIgnoreCase("Vista de compra")) {
+				
+			float precio_total= prod.getPrecio() * prod.getCantidad();
+			lista_prod.setText(next_row, next_col, String.valueOf(precio_total));
+			next_col++;
+			}
+			lista_prod.setWidget(next_row, next_col, btn_borrar);
+			
 //		lista_lineas.setText(next_row, 4, prod.obtener_nro_serie());
 //		if (lista.size() > 1) {
 //			check_quitar.setFormValue(linea.obtener_nro_cuenta() + " " +
@@ -67,4 +110,15 @@ public class WidgetMostrarProductos extends Composite {
 		principal.add(lista_prod);
 		initWidget(principal);
 	}
+	
+	void agregar_handler() {
+		btn_borrar.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				confirmar_borrado= new WidgetConfirmar(parent, prod_actual);
+				confirmar_borrado.show();
+			}
+			});
+	    }
 }
