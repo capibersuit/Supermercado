031d418d07f7ca8f9293c5531f7ff3a22420b4b2
capibersuit <christianferro79@gmail.com>
2015-12-23 17:23:27 -0300

    Agrego lo necesario para borrar y actualizar listas.

--- supermercado/src/ar/gov/chris/client/widgets/WidgetMostrarListas.java
+++ supermercado/src/ar/gov/chris/client/widgets/WidgetMostrarListas.java
@@ -3,6 +3,7 @@ package ar.gov.chris.client.widgets;
 import java.util.Set;
 
 import ar.gov.chris.client.datos.DatosLista;
+import ar.gov.chris.client.pantalla.PantallaListaDeCompras;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -22,6 +23,14 @@ public class WidgetMostrarListas extends Composite {
 	private int next_row;
 	
 	private PushButton btn_ir;
+	private PushButton btn_borrar;
+	private PushButton btn_actualizar;
+	
+	private WidgetConfirmar confirmar_borrado;
+	private WidgetAgregarLista actualizar_lista;
+
+	private PantallaListaDeCompras parent;
+
 	DatosLista lista;
 	
 	/** Crea un {@link WidgetMostrarLineas} a partir de los par�metros.
@@ -30,8 +39,9 @@ public class WidgetMostrarListas extends Composite {
 	 * @param titulo T�tulo del widget.
 	 */
 	public WidgetMostrarListas(final Set<DatosLista> lista, 
-			String titulo) {
-		
+			String titulo, final PantallaListaDeCompras parent) {
+		this.parent= parent;
+
 		principal= new FlowPanel();
 		listas= new FlexTable();
 		titulo_label= new Label(titulo);
@@ -44,10 +54,14 @@ public class WidgetMostrarListas extends Composite {
 		Label comentario_label= new Label("Comentario");
 		Label fecha_label= new Label("Fecha");
 		Label id_lista_label= new Label("Id lista");
+		Label borrar_label= new Label("Borrar");
+		Label actualizar_label= new Label("Actualizar");
 
 		listas.setWidget(0, 0, comentario_label);
 		listas.setWidget(0, 1, fecha_label);
 		listas.setWidget(0, 2, id_lista_label);
+		listas.setWidget(0, 3, borrar_label);
+		listas.setWidget(0, 4, actualizar_label);
 
 		next_row= 1; 
 
@@ -64,12 +78,49 @@ public class WidgetMostrarListas extends Composite {
 					History.fireCurrentHistoryState();;
 				}
 				});
+			
+			btn_borrar= new PushButton("Borrar");
+			btn_actualizar= new PushButton("Actualizar");
+			
+			final ClickHandler handler = new ClickHandler(){
+				public void onClick(ClickEvent arg0) {
+//					if(parent instanceof PantallaProductos)
+//						((PantallaProductos)parent).borrar_producto(prod.getNombre());
+//					else if(parent instanceof PantallaVistaDeCompra)
+					
+//						((PantallaVistaDeCompra)parent).borrar_lista(((PantallaVistaDeCompra)parent).getId_compra());
+						parent.borrar_lista(list.getId());
+
+//					hide();
+				}
+			};
+			
+			btn_borrar.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				confirmar_borrado= new WidgetConfirmar(parent, "Está seguro que quiere borrar la lista de compra", handler);
+				confirmar_borrado.show();
+			}
+			});
+			
+			btn_actualizar.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				actualizar_lista= new WidgetAgregarLista(parent, list);
+				actualizar_lista.show();
+			}
+			});
 //			agregar_handler();
 
 //			CheckBox check_quitar= new CheckBox("Quitar l�nea");
 			listas.setText(next_row, 0, list.getComentario());
 			listas.setText(next_row, 1, list.getFecha().toString());
 			listas.setWidget(next_row, 2, btn_ir);
+			listas.setWidget(next_row, 3, btn_borrar);
+			listas.setWidget(next_row, 4, btn_actualizar);
+
 //		lista_lineas.setText(next_row, 2, prod.obtener_marca());
 //		lista_lineas.setText(next_row, 3, prod.obtener_modelo());
 //		lista_lineas.setText(next_row, 4, prod.obtener_nro_serie());
