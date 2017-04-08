e491470e4f7fa2066c32b9309cf3c6ee0ad78799
capibersuit <christianferro79@gmail.com>
2017-04-07 21:40:31 -0300

    Clase que sirve para leer datos de un archivo de propiedades. (actualmente lo uso para levantar datos sobre el correo electronico que uso para enviar las alarmas de vencimiento)

---
+++ supermercado/src/ar/gov/chris/server/clases/LectorPropiedades.java
@@ -0,0 +1,65 @@
+package ar.gov.chris.server.clases;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import ar.gov.chris.server.excepciones.ExcepcionIO;
+
+/**
+ * Esta clase permite leer parámetros de un archivo de propiedades.
+ * @author christian
+ *
+ */
+public class LectorPropiedades {
+	
+	 private static Properties propiedades = new Properties();
+	// private static String ARCH_CONF="/supermercado/src/ar/gov/chris/server/clases/archivo.properties";
+
+	 private static String ARCH_CONF="E:\\supermercado.properties";
+	 
+//	 private static String ARCH_CONF="/usr/local/supermercado/bin/supermercado.properties";
+
+	 
+	 
+	/** Devuelve el valor asociado a una clave.
+	 * @param clave La clave.
+	 * @return El valor.
+	 * @throws GWT_ExcepcionIO Si hay algún problema con el archivo de propiedades.
+	 */
+	public static  String obtener_valor(String clave) throws ExcepcionIO {
+		String valor = "";
+		try {
+			// Intenta buscar el archivo de propiedades en la carpeta resources.
+			
+			// Deésta manera busca el archivo de conf en la ruta
+			// WEB-INF/clases/archivo.properties
+			// y éste se genera a partir del que está en la carpeta resources.
+			
+			FileInputStream inputStream = new FileInputStream(ARCH_CONF);
+
+			// Deésta manera busca el archivo de conf en la ruta
+			// WEB-INF/clases/ar/gov/mecon/properties/archivo.properties
+			// y éste se genera a partir del que está en la carpeta 
+			// src/ar/gov/mecon/properties.
+			
+//			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(ARCH_CONF);
+			// Si pude obtener el archivo de propiedades, entonces las cargo.
+			if (inputStream != null) {
+				propiedades.load(inputStream);
+			} else {
+				throw new ExcepcionIO("El archivo de propiedades '" + 
+						ARCH_CONF + "' no fue encontrado ó no se pudo leer." +
+						" Notifique a los administradores del sistema.");
+			}
+		} catch (IOException e) {
+			throw new ExcepcionIO("El archivo de propiedades '" + 
+					ARCH_CONF + "' no fue encontrado ó no se pudo leer." +
+					" Notifique a los administradores del sistema.");
+		}
+		// Obtengo el valor de la propiedad que me interesa.
+		valor= propiedades.getProperty(clave);
+		return valor;
+	}
+}
