44b935bfde99a68e9613f0c6c40e65d08daa2165
capibersuit <christianferro79@gmail.com>
2017-04-07 21:43:46 -0300

    actualizo al dia 07-04-2017

--- supermercado/src/BD/supermercado.sql
+++ supermercado/src/BD/supermercado.sql
@@ -7,6 +7,7 @@ CREATE TABLE productos
   id serial NOT NULL,
   nombre text,
   precio double precision,
+
   CONSTRAINT id_pk_productos PRIMARY KEY (id )
 )
 WITH (
@@ -45,6 +46,11 @@ CREATE TABLE rel_listas_productos
 
 ALTER TABLE rel_listas_productos ADD esta_marcada boolean
 
+ALTER TABLE rel_listas_productos ADD fecha_venc timestamp
+
+ALTER TABLE rel_listas_productos ADD existe_todavia boolean default true
+
+
 alter table rel_listas_productos
  add constraint FK_rel_listas_productos
   foreign key (id_prod)
@@ -54,6 +60,10 @@ alter table rel_listas_productos
  add constraint FK_rel_listas
   foreign key (id_compra)
   references listas(id);
+
+alter table rel_listas_productos
+  add constraint UQ_rel_listas
+  unique (id_prod, id_compra);
 
 --******************* HASTA ACA DEFINICIONES *****************************************
 
@@ -89,7 +99,9 @@ UPDATE rel_listas_productos
    SET esta_marcada= true, precio=99
  WHERE id_compra=15 and id_prod=98
 
+
 select * from rel_listas_productos
+	where id_prod = 185
 
 
 SELECT * FROM rel_listas_productos 
@@ -98,3 +110,147 @@ where id_compra = 3
 
 
 select * from productos where nombre = 'coca cola'
+
+
+---------------- 16-12-2016 --------------------------
+SELECT NOW();
+
+
+SELECT * FROM rel_listas_productos 
+
+	where fecha_venc < NOW()  + INTERVAL '30 DAY';
+
+
+
+update rel_listas_productos set fecha_venc = '2016-12-27'
+
+where id_compra = 24
+	and id_prod = 273 
+
+
+SELECT * FROM productos WHERE nombre ~* 'salchicha'
+
+
+
+SELECT * FROM rel_listas_productos rlp, productos p
+where rlp.id_prod = p.id
+and (id_compra in (24,23,22,20))
+	and nombre ~* 'carton'
+order by  id_compra , nombre desc
+
+
+SELECT * FROM rel_listas_productos rlp, productos p
+where rlp.id_prod = p.id
+and fecha_venc is not null
+
+
+
+SELECT * FROM rel_listas_productos rlp, productos p
+where rlp.id_prod = p.id
+and rlp.id_compra = 26
+
+order by p.nombre
+
+SELECT id, id_compra, id_prod, cant, precio, esta_marcada, fecha_venc, 
+       existe_todavia
+  FROM rel_listas_productos 
+	order by id_compra desc
+
+  
+update rel_listas_productos set existe_todavia = true 
+
+, fecha_venc = '2017-06-06'
+
+where id_compra = 26 
+	and id_prod = 141
+
+-- ---------------------- 03-02-2017 -----------------------------
+
+SELECT * FROM rel_listas_productos rlp, productos p
+where rlp.id_prod = p.id
+and fecha_venc is not null
+order by fecha_venc
+
+
+update rel_listas_productos set fecha_venc = '2017-02-13' where id_compra = 27 	and id_prod = 771 
+
+update rel_listas_productos set fecha_venc = '2017-02-25' where id_compra = 27 	and id_prod = 907
+
+update rel_listas_productos set fecha_venc = '2017-02-23' where id_compra = 27 	and id_prod = 906
+
+update rel_listas_productos set fecha_venc = '2017-02-23' where id_compra = 27 	and id_prod = 906
+
+update rel_listas_productos set fecha_venc = '2017-02-23' where id_compra = 27 	and id_prod = 906
+
+update rel_listas_productos set fecha_venc = '2017-02-26' where id_compra = 27 	and id_prod = 700
+
+update rel_listas_productos set fecha_venc = '2017-03-02' where id_compra = 27 	and id_prod = 69
+
+update rel_listas_productos set fecha_venc = '2017-05-25' , existe_todavia= true where id_compra = 23 	and id_prod = 197
+
+update rel_listas_productos set fecha_venc = '2017-06-18' , existe_todavia= true where id_compra = 26 	and id_prod = 197 
+
+update rel_listas_productos set fecha_venc = '2017-09-29' where id_compra = 27 	and id_prod = 68
+
+
+-- ---------------------- 10-02-2017 -----------------------------
+
+UPDATE rel_listas_productos
+   SET esta_marcada= false WHERE id_compra=27
+
+   
+
+SELECT rlp.id_compra, rlp.fecha_venc, rlp.existe_todavia, p.* FROM rel_listas_productos rlp, productos p
+where rlp.id_prod = p.id
+and fecha_venc is not null and  existe_todavia
+order by fecha_venc
+
+
+update rel_listas_productos set fecha_venc = '2017-06-06' where id_compra = 27 	and id_prod = 143
+update rel_listas_productos set fecha_venc = '2017-03-23' , existe_todavia= true where id_compra = 26 	and id_prod = 837
+update rel_listas_productos set fecha_venc = '2017-09-20' where id_compra = 27 	and id_prod = 697
+update rel_listas_productos set fecha_venc = '2017-06-25' where id_compra = 27 	and id_prod = 197
+
+update rel_listas_productos set fecha_venc = '2017-05-18', existe_todavia= true where id_compra = 26 	and id_prod = 687
+update rel_listas_productos set fecha_venc = '2017-09-26', existe_todavia= true where id_compra = 26 	and id_prod = 688
+update rel_listas_productos set fecha_venc = '2017-04-06', existe_todavia= true where id_compra = 24 	and id_prod = 143
+update rel_listas_productos set fecha_venc = '2017-06-24', existe_todavia= true where id_compra = 23 	and id_prod = 77
+update rel_listas_productos set fecha_venc = '2017-06-05', existe_todavia= true where id_compra = 24 	and id_prod = 822
+
+update rel_listas_productos set existe_todavia = false where id_compra = 26 and id_prod = 273 
+update rel_listas_productos set existe_todavia = false where id_compra = 26 and id_prod = 273 
+
+-- ---------------------- 16-02-2017 -----------------------------
+
+SELECT rlp.id_compra, rlp.fecha_venc, rlp.existe_todavia, p.* FROM rel_listas_productos rlp, productos p
+where rlp.id_prod = p.id
+and fecha_venc is not null and  existe_todavia
+order by fecha_venc
+
+
+update rel_listas_productos set existe_todavia = false where id_compra = 27 and id_prod = 771 
+update rel_listas_productos set existe_todavia = false where id_compra = 27 and id_prod = 906 
+update rel_listas_productos set existe_todavia = false where id_compra = 27 and id_prod = 907 
+update rel_listas_productos set existe_todavia = false where id_compra = 27 and id_prod = 69
+
+-- ---------------------- 04-03-2017 -----------------------------
+
+update rel_listas_productos set existe_todavia = false where id_compra = 27 and id_prod = 700 
+
+-- ---------------------- 22-03-2017 -----------------------------
+
+update rel_listas_productos set existe_todavia = false where id_compra = 26 and id_prod = 837 
+
+-- ---------------------- 06-04-2017 -----------------------------
+
+update listas set fecha = '01/03/2017' where id = 28
+update listas set fecha = '05/04/2017' where id = 29
+
+
+update rel_listas_productos set existe_todavia = false where id_compra = 24 and id_prod = 143
+
+
+
+
+
+
