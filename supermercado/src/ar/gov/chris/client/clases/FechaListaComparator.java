0eec75cf504a21dee4e5e82e56c1c42b613ebb0d
STUART <STUART@MERETUEW10>
2019-09-09 11:10:03 -0300

    Hago un cambio para poder ordenar las listas por fecha descendente!

--- supermercado/src/ar/gov/chris/client/clases/FechaListaComparator.java
+++ supermercado/src/ar/gov/chris/client/clases/FechaListaComparator.java
@@ -7,7 +7,7 @@ import ar.gov.chris.client.datos.DatosLista;
 public class FechaListaComparator implements Comparator<DatosLista>  {
 	@Override
 	  public int compare(DatosLista datos_1, DatosLista datos_2) {
-	    return datos_1.getFecha().compareTo(datos_2.getFecha());
+	    return datos_2.getFecha().compareTo(datos_1.getFecha());
 	  }
 
 }
