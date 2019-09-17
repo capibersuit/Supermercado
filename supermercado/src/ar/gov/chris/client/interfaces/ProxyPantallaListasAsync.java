5cb3e47e572d79ce116e231cbd0dceb8ea5b30d9
chris <chris@DESKTOP-IB4T4TL>
2019-09-17 17:37:06 -0300

    Ahora cuando una lista se graba devuelve el id.

--- supermercado/src/ar/gov/chris/client/interfaces/ProxyPantallaListasAsync.java
+++ supermercado/src/ar/gov/chris/client/interfaces/ProxyPantallaListasAsync.java
@@ -12,7 +12,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public interface ProxyPantallaListasAsync {
 	
 	void agregar_lista(DatosLista datos_list,
-			AsyncCallback<Void> asyncCallback);
+			AsyncCallback<Integer> asyncCallback);
 
 	void buscar_listas(AsyncCallback<Set<DatosLista>> asyncCallback);
 
