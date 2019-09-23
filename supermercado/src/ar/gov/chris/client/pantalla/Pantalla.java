36163fe524da7ac76a104206711b366fb077cf79
STUART <STUART@MERETUEW10>
2019-09-22 22:24:28 -0300

    Aumento la version a 4.3.

--- supermercado/src/ar/gov/chris/client/pantalla/Pantalla.java
+++ supermercado/src/ar/gov/chris/client/pantalla/Pantalla.java
@@ -29,7 +29,7 @@ public abstract class Pantalla extends Composite {
 	protected int anio_actual= anios.anio_actual();
 	protected int primer_anio_de_compras= anios.primer_anio_de_compras();
 	
-	protected static final String VERSION = "4.2";
+	protected static final String VERSION = "4.3";
 
 	
 	/** Constructor sin parámetros.
