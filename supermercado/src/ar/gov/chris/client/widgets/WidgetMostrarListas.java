eaedb2ea57105d76149d1c813b6804c3b4b51754
chris <chris@DESKTOP-IB4T4TL>
2019-10-09 13:07:51 -0300

    Finalmente logré poder subtotalizar las compras !!!
    AUNQUE, con ciertos reparos....
    
    - No me totaliza la compra mas antigua (enero del 2016!)
    - Si por ejemplo, no hay compra en un año en el mes de diciembre, me
    agrega lineas de total de mes en 0 arriba del ultimo mes... y alguna
    otra.

--- supermercado/src/ar/gov/chris/client/widgets/WidgetMostrarListas.java
+++ supermercado/src/ar/gov/chris/client/widgets/WidgetMostrarListas.java
@@ -34,8 +34,6 @@ public class WidgetMostrarListas extends Composite {
 	private WidgetConfirmar confirmar_borrado;
 	private WidgetAgregarLista actualizar_lista;
 
-	private PantallaListaDeCompras parent;
-
 	DatosLista lista;
 	
 	String[] meses_str = new String[]{
@@ -52,7 +50,8 @@ public class WidgetMostrarListas extends Composite {
 			"Noviembre",
 			"Diciembre"};
 	
-	
+	private static final int ID_MES_VIERNES_EN_LA_LISTA = 11;
+
 
 	/** Crea un {@link WidgetMostrarLineas} a partir de los par�metros.
 	 * 
@@ -62,83 +61,13 @@ public class WidgetMostrarListas extends Composite {
 	public WidgetMostrarListas(final Set<DatosLista> lista, 
 			String titulo, final PantallaListaDeCompras parent, Date fecha_desde, Date fecha_hasta) {
 		
-		String enero= "ENERO";
-		String febrero= "FEBRERO";
-		String marzo= "MARZO";
-		String abril= "ABRIL";
-		String mayo= "MAYO";
-		String junio= "JUNIO";
-		String julio= "JULIO";
-		String agosto= "AGOSTO";
-		String septiembre= "SEPTIEMBRE";
-		String octubre= "OCTUBRE";
-		String noviembre= "NOVIEMBRE";
-		String diciembre= "DICIEMBRE";
-		
-		Label ene_label= new Label("Ene");
-		Label feb_label= new Label("Feb");
-		Label mar_label= new Label("Mar");
-		Label abr_label= new Label("Abr");
-		Label may_label= new Label("May");
-		Label jun_label= new Label("Jun");
-		Label jul_label= new Label("Jul");
-		Label ago_label= new Label("Ago");
-		Label sep_label= new Label("Sep");
-		Label oct_label= new Label("Oct");
-		Label nov_label= new Label("Nov");
-		Label dic_label= new Label("Dic");
-		
-		
-//		String[] meses_strasdsad = new String[]{
-//				"Diciembre",
-//				"Noviembre",
-//				"Octubre",
-//				"Septiembre",
-//				"Agosto",
-//				"Julio",
-//				"Junio",
-//				"Mayo",
-//				"Abril",
-//				"Marzo",
-//				"Febebro",
-//				"Enero"};
-//		
-//		String[] meses_str = new String[]{
-//				"Enero",
-//				"Febebro",
-//				"Marzo",
-//				"Abril",
-//				"Mayo",
-//				"Junio",
-//				"Julio",
-//				"Agosto",
-//				"Septiembre",
-//				"Octubre",
-//				"Noviembre",
-//				"Diciembre"};
-		
-		
-		Boolean[] meses_ya_usados = new Boolean[12];
+  		Boolean[] meses_ya_usados = new Boolean[12];
 		Arrays.fill(meses_ya_usados, Boolean.FALSE);
-		
-		int indice_mes=11;
-		
-		
-		
-		
-		
-		this.parent= parent;
-
+				
 		principal= new FlowPanel();
 		listas= new FlexTable();
 		titulo_label= new Label(titulo);
 		titulo_label.addStyleName("LabelDistinguido");
-		//		btn_ir = new PushButton(new Image("C:\Users\STUART\Downloads\boton-ir.png"));
-	
-//		Image imagen= new Image("/boton-ir.png");
-		
-		//		btn_ir = new PushButton("Ver lista");
-		//		btn_ir = new PushButton(new Image("/supermercado/src/ar/gov/chris/client/widgets/boton-ir.png"));
 
 		Label id_compra_label= new Label("Id");
 		Label comentario_label= new Label("Comentario");
@@ -150,26 +79,18 @@ public class WidgetMostrarListas extends Composite {
 
 		Label pagado_literal_label= new Label("Pagado");
 
-
 		Label id_lista_label= new Label("Ir");
 		Label borrar_label= new Label("B");
 		Label actualizar_label= new Label("A");
 
 		listas.setWidget(0, 0, id_compra_label);
-
-		listas.setWidget(0, 1, comentario_label);
-		
+		listas.setWidget(0, 1, comentario_label);		
 		listas.setWidget(0, 2, desc_label);
-
 		listas.setWidget(0, 3, super_label);
-
 		listas.setWidget(0, 4, suc_label);
-
-		listas.setWidget(0, 5, fecha_label);
-		
+		listas.setWidget(0, 5, fecha_label);		
 		listas.setWidget(0, 6, pagado_literal_label);
-		listas.setWidget(0, 7, id_lista_label);
-		
+		listas.setWidget(0, 7, id_lista_label);		
 		listas.setWidget(0, 8, borrar_label);
 		listas.setWidget(0, 9, actualizar_label);
 
@@ -179,60 +100,68 @@ public class WidgetMostrarListas extends Composite {
 		
 		float total_pagado_mes_actual=0;
 
+		Date fecha_ultima_compra= lista_ordenada.getFirst().getFecha();
+		
+		int mes_compra_anterior=-1;
+				
+		//xxx: Aca obtengo la primera compra en realidad, o sea la mas antigua (actualmente 2016-01-27!)
+		// 		creo que quise decir la ultima de la lista ordenada....
+		DatosLista ultima_compra= lista_ordenada.getLast();
 
 		for (final DatosLista list : lista_ordenada) {
-			
-			//**********************
-			
+						
 			Label total_pagado_mes_actual_label =new Label("0");
 			
 			total_pagado_mes_actual_label.addStyleName("MesSeparador");
-
+			
+			Label total_literal_label= new Label("TOTAL DEL MES");
+			total_literal_label.addStyleName("TotalDelMes");
 
 			String total_pagado_mes_actua_str="0";
-			
-
-			boolean reseteador_total_listas_por_mes=false;
-			//*************************
-
-			//			this.lista= list;
-			int mes_compra_anterior=-1;
+		
+			//XXX: No me quedó bien claro porque con este if logro que me aparezca el total de enero
+			//	   en cada año (salvo en 2016 por ser el primero)
+			if(ultima_compra.equals(list)) {
+				
+				total_pagado_mes_actua_str= String.valueOf(Mate.poner_dos_decimales(total_pagado_mes_actual));
+				total_pagado_mes_actual_label.setText(total_pagado_mes_actua_str);
+				listas.setWidget(next_row, 5, total_literal_label );
+				listas.setWidget(next_row, 6, total_pagado_mes_actual_label );
+			}
+				
 
 			if(fecha_desde.compareTo(list.getFecha()) < 0 && fecha_hasta.compareTo(list.getFecha()) > 0) {
 				
-//				int mes_compra_anterior=-1;
 				float pagado_compra_actual= list.getPagado();
 				
 				int mes_compra_actual=list.getFecha().getMonth();
 				
-//				if(total_pagado_mes_actual==0)
-					total_pagado_mes_actual+= pagado_compra_actual;
-
+				Date fecha_compra_actual= list.getFecha();
 				
-				if(!meses_ya_usados[mes_compra_actual]) {
-					
-					
+				total_pagado_mes_actual+= pagado_compra_actual;
 
-//					separador_meses(mes_compra_actual, total_pagado_mes_actual_label);
-					separador_meses(mes_compra_actual);
+											
+				if(mes_compra_actual!= mes_compra_anterior && !fecha_compra_actual.equals(fecha_ultima_compra) && mes_compra_actual!=ID_MES_VIERNES_EN_LA_LISTA) {
 
-					meses_ya_usados[mes_compra_actual]= true;	
+					total_pagado_mes_actual= total_pagado_mes_actual-pagado_compra_actual;
 					
-				} 
-				
-				if(mes_compra_actual!= mes_compra_anterior) {
 					total_pagado_mes_actua_str= String.valueOf(Mate.poner_dos_decimales(total_pagado_mes_actual));
 					total_pagado_mes_actual_label.setText(total_pagado_mes_actua_str);
 					
+					listas.setWidget(next_row, 5, total_literal_label );
 					listas.setWidget(next_row, 6, total_pagado_mes_actual_label );
-
 					
 					next_row++;
 
 					total_pagado_mes_actual=pagado_compra_actual;
-					mes_compra_actual= mes_compra_anterior;
-
+					mes_compra_anterior=mes_compra_actual;
 				}
+				
+				if(!meses_ya_usados[mes_compra_actual]) {									
+					separador_meses(mes_compra_actual);
+					meses_ya_usados[mes_compra_actual]= true;						
+				} 
+				
 				Image imagen_boton_ir= new Image("imagenes/boton-ir.jpg");
 				imagen_boton_ir.setAltText(""+list.getId());
 				btn_ir = new PushButton(imagen_boton_ir);
@@ -262,18 +191,9 @@ public class WidgetMostrarListas extends Composite {
 				
 				btn_actualizar= new PushButton(imagen_boton_actualizar);
 				
-				
-
 				final ClickHandler handler = new ClickHandler(){
 					public void onClick(ClickEvent arg0) {
-						//					if(parent instanceof PantallaProductos)
-						//						((PantallaProductos)parent).borrar_producto(prod.getNombre());
-						//					else if(parent instanceof PantallaVistaDeCompra)
-
-						//						((PantallaVistaDeCompra)parent).borrar_lista(((PantallaVistaDeCompra)parent).getId_compra());
 						parent.borrar_lista(list.getId());
-
-						//					hide();
 					}
 				};
 
@@ -294,7 +214,6 @@ public class WidgetMostrarListas extends Composite {
 						actualizar_lista.show();
 					}
 				});
-				//			agregar_handler();
 
 				//			CheckBox check_quitar= new CheckBox("Quitar lineas");
 				listas.setText(next_row, 0, String.valueOf(list.getId()));
@@ -321,9 +240,7 @@ public class WidgetMostrarListas extends Composite {
 
 				pagado_label.setText(pagado_str);
 
-				listas.setWidget(next_row, 6,  pagado_label);
-
-
+				listas.setWidget(next_row, 6, pagado_label);
 				listas.setWidget(next_row, 7, btn_ir);
 				listas.setWidget(next_row, 8, btn_borrar);
 				listas.setWidget(next_row, 9, btn_actualizar);
@@ -351,19 +268,12 @@ public class WidgetMostrarListas extends Composite {
 		initWidget(principal);
 	}
 	
-	private void separador_meses(int mes/*, Label total_pagado_mes_actual_label*/) {
-		
+	private void separador_meses(int mes) {		
 		Label mes_separador =new Label();
-		mes_separador.addStyleName("MesSeparador");
-		
-//		listas.setText(next_row, 5,  meses_str[mes]);
+		mes_separador.addStyleName("MesSeparador");		
 		
 		mes_separador.setText(meses_str[mes]);
-
-		listas.setWidget(next_row, 5,  mes_separador);
-//		listas.setWidget(next_row, 6, total_pagado_mes_actual_label );
-
-		
+		listas.setWidget(next_row, 5,  mes_separador);		
 		next_row++;
 	}
 }
